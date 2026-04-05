8f15f5846a0a89074692613913a1c0cdd007ac78
abbas <syedabbas7401@gmail.com>
2026-04-05 14:10:26 +0530

    fix: Work-Image capital I

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,9 +4,9 @@ import "./Work.css";
 
 /* IMAGE IMPORTS */
 
-import treeImg from "../assets/Work-image/Tree-Planation.png";
-import childrenImg from "../assets/Work-image/children.png";
-import personImg from "../assets/Work-image/person.png";
+import treeImg from "../assets/Work-Image/Tree-Planation.png";
+import childrenImg from "../assets/Work-Image/children.png";
+import personImg from "../assets/Work-Image/person.png";
 
 const workData = [
   {
